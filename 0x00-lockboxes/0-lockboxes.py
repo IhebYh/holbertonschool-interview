f84a18d7a72b0ac4ee16fe4845bd4c6a21bad3ed
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-08-03 22:04:04 +0100

    change from list to ennumerate to write less code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,17 +7,13 @@ each box may contain keys to the other boxes.
 
 
 def canUnlockAll(boxes):
+    """ lockboxes problem """
     boxesNbr = len(boxes) - 1
     unlockedBoxes = []
-    for i in range(0, boxesNbr):
-        if type(boxes[i]) == list:
-            for j in range(0, len(boxes[i])):
-                if boxes[i][j] >= 0 and boxes[i][j] <= boxesNbr:
-                    if not unlockedBoxes.__contains__(boxes[i][j]):
-                        unlockedBoxes.append(boxes[i][j])
-        else:
-            if boxes[i] >= 0 and boxes[i] <= boxesNbr:
-                if not unlockedBoxes.__contains__(boxes[i]):
-                    unlockedBoxes.append(boxes[i])
+    for i, box in enumerate(boxes):
+        for key in box:
+            if key <= boxesNbr:
+                if not unlockedBoxes.__contains__(key):
+                    unlockedBoxes.append(key)
     canUnlock = len(unlockedBoxes) == (boxesNbr) or len(unlockedBoxes) == (boxesNbr + 1)
     return canUnlock
