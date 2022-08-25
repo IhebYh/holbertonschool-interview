136c531df0803be0810ea437c98608d8d4b2bb5a
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-08-25 22:53:02 +0100

    fixed

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -119,7 +119,6 @@ heap_t *heap_insert(heap_t **root, int value)
 		if (node != NULL)
 			break;
 	}
-	//_sort(node);
 	while (node->parent != NULL && node->n > node->parent->n)
 	{
 		tmp = node->parent->n;
