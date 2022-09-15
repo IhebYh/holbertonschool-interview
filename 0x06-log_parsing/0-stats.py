1d308867b7d6445a66fdf7ff49637d5e7591ab1a
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-09-16 00:01:24 +0100

    fixed

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,6 +10,7 @@ def handler(sig, frame):
         statuses[status] = 0
     sys.exit(0)
 
+
 counter = 0
 fileSize = 0
 statuses = {'200': 0,
@@ -21,16 +22,16 @@ statuses = {'200': 0,
             }
 signal.signal(signal.SIGINT, handler)
 while sys.stdin.readable():
-   line = sys.stdin.readline()
-   lineSplitted = line.split(" ")
-   fileSize += int(lineSplitted[-1])
-   if statuses.__contains__(lineSplitted[-2]):
+    line = sys.stdin.readline()
+    lineSplitted = line.split(" ")
+    fileSize += int(lineSplitted[-1])
+    if statuses.__contains__(lineSplitted[-2]):
         statuses[lineSplitted[-2]] += 1
         counter += 1
-   if counter == 10:
-    print("File size: {}".format(fileSize))
-    for status in statuses:
-        print("{}: {}".format(status, statuses[status]))
-        statuses[status] = 0
-    counter = 0
-    fileSize = 0
+    if counter == 10:
+        print("File size: {}".format(fileSize))
+        for status in statuses:
+            print("{}: {}".format(status, statuses[status]))
+            statuses[status] = 0
+        counter = 0
+        fileSize = 0
