113422ae340da0add5c9b784dadfe08f567fe58c
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-10-22 21:06:46 +0100

    module documented

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,25 +2,24 @@
 import sys
 import signal
 '''Module for log parsing script.'''
+if __name__ == "__main__":
+    counter = 0
+    fileSize = 0
+    statuses = {'200': 0,
+                '401': 0,
+                '403': 0,
+                '404': 0,
+                '405': 0,
+                '500': 0
+                }
+    def handler(sig, frame):
+        ''' Log handler '''
+        print("File size: {}".format(fileSize))
+        for status in statuses:
+            print("{}: {}".format(status, statuses[status]))
+            statuses[status] = 0
+        sys.exit(0)
 
-def handler(sig, frame):
-    ''' Log handler '''
-    print("File size: {}".format(fileSize))
-    for status in statuses:
-        print("{}: {}".format(status, statuses[status]))
-        statuses[status] = 0
-    sys.exit(0)
-
-
-counter = 0
-fileSize = 0
-statuses = {'200': 0,
-            '401': 0,
-            '403': 0,
-            '404': 0,
-            '405': 0,
-            '500': 0
-            }
 signal.signal(signal.SIGINT, handler)
 while sys.stdin.readable():
     line = sys.stdin.readline()
