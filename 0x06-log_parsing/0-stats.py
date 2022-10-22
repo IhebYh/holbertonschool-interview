84164f9bd347075a0a55c57d01cec2820d4eb857
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-10-22 21:13:03 +0100

    documentation finished

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -23,18 +23,18 @@ if __name__ == "__main__":
             statuses[status] = 0
         sys.exit(0)
 
-signal.signal(signal.SIGINT, handler)
-while sys.stdin.readable():
-    line = sys.stdin.readline()
-    lineSplitted = line.split(" ")
-    fileSize += int(lineSplitted[-1])
-    if statuses.__contains__(lineSplitted[-2]):
-        statuses[lineSplitted[-2]] += 1
-        counter += 1
-    if counter == 10:
-        print("File size: {}".format(fileSize))
-        for status in statuses:
-            print("{}: {}".format(status, statuses[status]))
-            statuses[status] = 0
-        counter = 0
-        fileSize = 0
+    signal.signal(signal.SIGINT, handler)
+    while sys.stdin.readable():
+        line = sys.stdin.readline()
+        lineSplitted = line.split(" ")
+        fileSize += int(lineSplitted[-1])
+        if statuses.__contains__(lineSplitted[-2]):
+            statuses[lineSplitted[-2]] += 1
+            counter += 1
+        if counter == 10:
+            print("File size: {}".format(fileSize))
+            for status in statuses:
+                print("{}: {}".format(status, statuses[status]))
+                statuses[status] = 0
+            counter = 0
+            fileSize = 0
