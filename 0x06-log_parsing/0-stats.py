8a43f475ad88e7350c8bb84aaec3ab94e6047276
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-10-22 21:37:40 +0100

    returnin to the old solution

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
 '''Module for log parsing script.'''
+
 import sys
-from tkinter import E
+import signal
 
 if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    counter = 0
+    fileSize = 0
+    statuses = {'200': 0,
+                '401': 0,
+                '403': 0,
+                '404': 0,
+                '405': 0,
+                '500': 0
+                }
 
-    def check_match(line):
-        '''Checks for regexp match in line.'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except Exception:
-            pass
+    def handler(sig, frame):
+        ''' Log handler '''
+        print("File size: {}".format(fileSize))
+        for status in statuses:
+            print("{}: {}".format(status, statuses[status]))
+            statuses[status] = 0
+        sys.exit(0)
 
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+    signal.signal(signal.SIGINT, handler)
+    while sys.stdin.readable():
+        line = sys.stdin.readline()
+        lineSplitted = line.split(" ")
+        fileSize += int(lineSplitted[-1])
+        if statuses.__contains__(lineSplitted[-2]):
+            statuses[lineSplitted[-2]] += 1
+            counter += 1
+        if counter == 10:
+            print("File size: {}".format(fileSize))
+            for status in statuses:
+                print("{}: {}".format(status, statuses[status]))
+                statuses[status] = 0
+            counter = 0
+            fileSize = 0
