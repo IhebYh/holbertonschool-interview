5756e937530939012be65abf33247e2b878f472e
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-01-10 20:08:46 +0100

    var renamed

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -2,6 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,11 +22,11 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
-int heap_extract(heap_t **root);
-size_t binary_tree_height(heap_t *root);
-void find_replacement(heap_t *root, size_t level, heap_t **last);
-void free_and_replace(heap_t **root, heap_t **left,
-		      heap_t **right, heap_t **last);
-void heapify(heap_t **root, heap_t *current, int *check);
+int heap_extract(binary_tree_t **root);
+size_t binary_tree_height(binary_tree_t *root);
+void find_replacement(binary_tree_t *root, size_t level, binary_tree_t **last);
+void free_and_replace(binary_tree_t **root, binary_tree_t **left,
+		      binary_tree_t **right, binary_tree_t **last);
+void heapify(binary_tree_t **root, binary_tree_t *current, int *check);
 
 #endif /* _BINARY_TREES_H_ */
