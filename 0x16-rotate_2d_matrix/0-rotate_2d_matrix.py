5611c83dc3dc7d236b02dac695bdff043c75d430
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-01-19 21:32:16 +0100

    rotate 2d matrix 90° clockwise in place

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,10 +3,23 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """ Given an n x n 2D matrix,
     rotate it 90 degrees clockwise """
-    for i in range (0,4):
-    for j in range(0,4):
-        X[i][j] = Y[j][i]
-    list.reverse(X[i])
+    n = len(matrix)
+    temp1, temp2 = 0, 0
+
+    for j in range(0, len(matrix) // 2 + 1):
+        for i in range(j, n - 1):
+            temp1 = matrix[i][n - 1]
+            matrix[i][n - 1] = matrix[j][i]
+
+            temp2 = matrix[n - 1][n - 1 - i + j]
+            matrix[n - 1][n - 1 - i + j] = temp1
+
+            temp1 = matrix[n - 1 - i + j][j]
+            matrix[n - 1 - i + j][j] = temp2
+
+            matrix[j][i] = temp1
+        n -= 1
