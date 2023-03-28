5c0354dc5717906ce25f345869402bc34b5d5377
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-03-28 22:54:25 +0100

    pep8 doc style fixed

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,16 +4,17 @@
 Island perimeter
 """
 
+
 def island_perimeter(grid):
     """ island perimeter """
-    m,n=len(grid), len(grid[0])
-    land,neighbor= 0,0
+    m, n = len(grid), len(grid[0])
+    land, neighbor = 0, 0
     for i in range(m):
         for j in range(n):
-            if grid[i][j]==1:
-                land+=1
-                if i < m-1 and grid[i+1][j]==1:
-                    neighbor+=1
-                if j < n-1 and grid[i][j+1]==1:
-                    neighbor+=1
-    return land*4-2*neighbor
+            if grid[i][j] == 1:
+                land += 1
+                if i < m-1 and grid[i+1][j] == 1:
+                    neighbor += 1
+                if j < n-1 and grid[i][j+1] == 1:
+                    neighbor += 1
+    return land * 4 - 2 * neighbor
