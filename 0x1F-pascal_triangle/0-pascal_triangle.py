c8a5962814a44b6aab8615e3c19508d980eeae53
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-04-05 23:46:36 +0100

    fixed

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from math import factorial
+"""
+Pascal triangle module
+"""
+
+def pascal_triangle(n):
+    """ pascal triangle """
+    pascal = [[1]]
+    for _ in range(n - 1):
+        pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
+                   for i in range(len(pascal[-1]) - 1)] + [1]]
+    return pascal if n > 0 else []
