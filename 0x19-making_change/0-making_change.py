e1ab16642daa7e247c889128f72c786501d27cf5
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-04-06 22:50:47 +0100

    commit the second solution after checking the QA Review

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,22 +3,15 @@
 
 def makeChange(coins, total):
     """ Make Change Module """
-    if total == 0 : 
+    if total <= 0:
         return 0
-    total_coins = sum(coins)
-    if total_coins > total:
-        return -1
-    coins_tmp = coins
-    coins_tmp.sort()
-    still_counting = True
-    change = 1
-    while (still_counting):
-        if total_coins == total or not coins_tmp:
-            change += 1
-            still_counting = False
-        elif total_coins + coins_tmp[-1] <= total:
-            change += 1
-            total_coins += coins_tmp[-1]
-        else :
-            coins_tmp.pop()
-    return change
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    coins.sort()
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dp[i - coin] != -1:
+                dp[i] = min(dp[i - coin] + 1, dp[i])
+    return dp[total] if dp[total] != float('inf') else -1
